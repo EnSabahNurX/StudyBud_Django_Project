1162b03771f282db86419b00a936e64a458eab19
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-10-15 21:02:41 +0100

    setting rooms and access to rooms from home

--- base/views.py
+++ base/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {"id": 1, "name": "Lets learn Python"},
+    {"id": 2, "name": "Lets learn Javascript"},
+    {"id": 3, "name": "Lets learn Html"},
+    {"id": 4, "name": "Lets learn Css"},
+    {"id": 5, "name": "Lets learn Django"},
+]
+
+
 def home(request):
-    return render(request, "index.html")
+    context = {"rooms": rooms}
+    return render(request, "base/index.html", context)
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, "base/room.html", context)
