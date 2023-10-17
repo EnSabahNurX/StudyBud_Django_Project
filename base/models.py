949a99a1ac86f6aad3b0be8aabae74cf80b3d0da
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-10-17 19:18:22 +0100

    creating class in base

--- base/models.py
+++ base/models.py
@@ -6,3 +6,9 @@ class Room(models.Model):
     # topic=
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
