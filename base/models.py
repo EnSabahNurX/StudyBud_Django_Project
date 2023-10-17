d95227b9b113508f004df26ce8058f4810de4801
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-10-17 19:13:36 +0100

    creating class in base

--- base/models.py
+++ base/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+
+class Room(models.Model):
+    # host=
+    # topic=
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
