09479f0a7590fa9fda71443f1603e60efa729d21
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-10-21 22:13:29 +0100

    chat message room CRUD

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -75,7 +75,15 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all().order_by("-created")
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get("body"),
+        )
+
     context = {"room": room, "room_messages": room_messages}
     return render(request, "base/room.html", context)
 
