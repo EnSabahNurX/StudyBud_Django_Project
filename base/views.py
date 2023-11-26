a64760019b96fbf45c977d141a476a3c49857537
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-11-26 22:01:13 +0000

    Adjusting the urls inside new theme also loops

--- base/views.py
+++ base/views.py
@@ -117,13 +117,21 @@ def createRoom(request):
     form = RoomForm()
     topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect("home")
-    context = {"form": form,'topics':topics,}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
+        )
+
+        return redirect("home")
+    context = {
+        "form": form,
+        "topics": topics,
+    }
     return render(request, "base/room_form.html", context)
 
 
@@ -131,7 +139,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not allowed here!")
 
@@ -143,7 +151,10 @@ def updateRoom(request, pk):
         if form.is_valid():
             form.save()
             return redirect("home")
-    context = {"form": form}
+    context = {
+        "form": form,
+        "topics": topics,
+    }
     return render(request, "base/room_form.html", context)
 
 
