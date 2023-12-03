a10224f47208737a1e05d60102043fac0c9a017f
EnSabahNurX <ricklimadealmeida@gmail.com>
2023-12-03 21:12:36 +0000

    implementing Django REST Framework, urls and views

--- base/api/views.py
+++ base/api/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 
 def getRoutes(request):
     routes = [
+        "GET /api",
         "GET /api/rooms",
+        "GET /api/rooms/:id",
     ]
-    return JsonResponse()
+    return JsonResponse(routes, safe=False)
